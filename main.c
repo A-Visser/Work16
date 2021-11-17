93044b3cc92d3a7e4e23b3cd2488ba94d56c8bfb
Aaron Visser <avisser20@stuy.edu>
2021-11-16 22:42:40 -0500

    changes

--- main.c
+++ main.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <time.h>
 
+
+int main(){
+  printf("This is the parent speaking \n");
+  int status, f;
+  f = fork();
+  srand(time(NULL));
+  if(f) {
+    f = fork();
+  }
+  if(f) {
+    int w, status;
+    w = wait(&status);
+    printf("child: %d\tasleep for %d seconds\n", w, WEXITSTATUS(status));
+    printf("End of parent\n");
+    printf("End of program\n");
+  } else {
+    printf("Child PID:%d \n", getpid());
+    float r = (rand()%1000/(float)1000)*3 +2;
+    sleep(r);
+    printf("Child is finished \n");
+    return r;
+  }
+  return 0;
+}
